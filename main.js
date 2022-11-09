279409ea0e92deb00da13c0b11730d3d63866809
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-09 16:27:56 -0500

    Add randomFunc constant

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
 // GEnerator functions - https://www.net-comber.com/charset.html
 
 function getRandomLower() {
-    /* 26 is the amount of letter in the english and french alphabet
+  /* 26 is the amount of letter in the english and french alphabet
     - spanish have 27 - italian 21 and german 26 but includes ä, ö and ß */
-    // 97 is the code for the start of the lower case letter in the charset(between 97 and 122)
+  // 97 is the code for the start of the lower case letter in the charset(between 97 and 122)
   return String.fromCharCode(Math.floor(Math.random * 26) + 97);
 }
 
-function getRandomUpper() { 
+function getRandomUpper() {
   // 65 is the code for the start of the upper case letter in the charset(between 65 and 90)
   return String.fromCharCode(Math.floor(Math.random * 26) + 65);
 }
@@ -19,6 +26,6 @@ function getRandomNumber() {
 }
 
 function getRandomSymbol() {
-    const symbols = '!@#$%^&*(){}[]=<>/,.+-_';  
+  const symbols = "!@#$%^&*(){}[]=<>/,.+-_";
   return symbols[Math.floor(Math.random * symbols.length)];
 }
