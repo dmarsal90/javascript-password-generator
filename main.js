f024540921b153b2d9b740d65dc167d84bae895f
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-09 16:17:31 -0500

    Add function getRandomNumber

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ function getRandomLower() {
 }
 
 function getRandomUpper() { 
-  // 65 is the code for the start of the upper case letter in the charset(between 97 and 122)
+  // 65 is the code for the start of the upper case letter in the charset(between 65 and 90)
   return String.fromCharCode(Math.floor(Math.random * 26) + 65);
+}
+
+function getRandomNumber() {
+  //10 is the amount of numbers
+  // 48 is the code for the start of the numbers in the charset(between 48 and 57)
+  return String.fromCharCode(Math.floor(Math.random * 10) + 48);
 }
