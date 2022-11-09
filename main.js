e55d10326d1f0ef137ed4bd8de22ef33348c1b9d
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-09 16:10:21 -0500

    Update getRandomLower function

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // GEnerator functions - https://www.net-comber.com/charset.html
 
 function getRandomLower() {
-    
+    /* 26 is the amount of letter in the english and french alphabet
+    - spanish have 27 - italian 21 and german 26 but includes ä, ö and ß */
+    // 97 is the code for the start of the lower cases in the charset(between 97 and 122)
+  return String.fromCharCode(Math.floor(Math.random * 26) + 97);
 }
