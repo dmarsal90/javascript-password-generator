bace508e65de2712b806db62274c8ee82617f35c
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-09 16:12:45 -0500

    Add getRandomUpper function

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 function getRandomLower() {
     /* 26 is the amount of letter in the english and french alphabet
     - spanish have 27 - italian 21 and german 26 but includes ä, ö and ß */
-    // 97 is the code for the start of the lower cases in the charset(between 97 and 122)
+    // 97 is the code for the start of the lower case letter in the charset(between 97 and 122)
   return String.fromCharCode(Math.floor(Math.random * 26) + 97);
+}
+
+function getRandomUpper() { 
+  // 65 is the code for the start of the upper case letter in the charset(between 97 and 122)
+  return String.fromCharCode(Math.floor(Math.random * 26) + 65);
 }
